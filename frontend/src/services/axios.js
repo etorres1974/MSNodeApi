7fa9b416cca8cecaa1fd4363b635733c385c5392
Eduardo Torres Ferreira <eduardotorres@jera.com.br>
2021-10-12 08:10:22 -0400

    Vue axios instance

---
+++ frontend/src/services/axios.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+
+const instance = axios.create({
+    baseURL: process.env.API_GATEWAY_PORT || 'http://localhost:8081/',
+    timeout: 1000,
+    headers: {
+        'X-Custom-Header': 'foobar',
+        'Access-Control-Allow-Origin' : '*',
+        'Content-Type' : 'application/json;charset=utf-8'
+    }
+  });
+
+export default instance 
